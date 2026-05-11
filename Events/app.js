b25c985fe710b4e755ba83d2e8d5c593c9db091f
Gautam Thakur <mnrhag5@gmail.com>
2026-05-11 12:57:04 +0530

    ad

--- Events/app.js
+++ Events/app.js
@@ -1,3 +1,13 @@
+
+//=====================================
+//=====================================
+//===============CLICK EVENT=================
+//=====================================
+//=====================================
+
+
+
+
 // let h1  = document.createElement("h1")
 // let btn = document.createElement("button");
 // let btn2 = document.createElement("button");
@@ -22,6 +32,19 @@
 // document.body.style.color = "black"
 // })
  
+
+
+
+
+
+
+//============================================
+//============================================
+//================  input ============================
+//============================================
+//============================================
+
+
 //  let input = document.querySelector("#yoo");
 //  input.addEventListener("input",(event)=>{
 //     console.log(input.value);
@@ -49,22 +72,120 @@
 
 
 
-let input = document.createElement("input");
-let h1 = document.createElement("h1");
+// let input = document.createElement("input");
+// let h1 = document.createElement("h1");
  
-document.body.appendChild(input);
-document.body.appendChild(h1);
+// document.body.appendChild(input);
+// document.body.appendChild(h1);
 
-input.addEventListener("input",()=>{
-    h1.innerText = input.value;
-if(h1.innerText=="red"){
-    h1.style.color = "red"
-}
-if(h1.innerText=="blue"){
-    h1.style.color = "blue"
-}
-if(h1.innerText==""){
-    h1.style.color = "black"
-}
-});
+// input.addEventListener("input",()=>{
+//     h1.innerText = input.value;
+// if(h1.innerText=="red"){
+//     h1.style.color = "red"
+// }
+// else if(h1.innerText=="blue"){
+//     h1.style.color = "blue"
+// }
+// else{
+//     h1.style.color = "black"
+// }
+// });
+
+
+
+
+
+
+//=======================================================
+//=======================================================
+//=======================================================
+//===========================SUBMIT======================
+//=======================================================
+//=======================================================
+//=======================================================
+
+
+// let form = document.createElement("form")
+// let input = document.createElement("input")
+// let button = document.createElement("button")
+// let h1 = document.createElement("h1")
+
+
+// form.appendChild(input)
+// form.appendChild(button)
+// form.appendChild(h1)
+
+// document.body.appendChild(form)
+
+// button.innerText = "submit"
+
+// form.addEventListener("submit",(e)=>{
+//     e.preventDefault();
+//     h1.innerText = input.value
+//         input.value=""
+    
+
+
+// })
+
+
+
+
+// let form = document.createElement("form");
+
+// let input = document.createElement("input");
+
+// let button = document.createElement("button");
+
+// let h1 = document.createElement("h1");
+
+// button.innerText = "Submit";
+
+// form.appendChild(input);
+
+// form.appendChild(button);
+
+// document.body.appendChild(form);
+
+// document.body.appendChild(h1);
+
+// form.addEventListener("submit",(e)=>{
+
+//     e.preventDefault();
+
+//     h1.innerText = input.value;
+
+//     input.value = "";
+
+// });
+
+
+
+let form = document.createElement("form")
+let input = document.createElement("input")
+let button = document.createElement("button")
+let ul = document.createElement("ul")
+let li = document.createElement("li")
+
+button.innerText = "Add";
+
+form.appendChild(input)
+form.appendChild(button)
+form.appendChild(ul) 
+
+
+document.body.appendChild(form)
+document.body.appendChild(ul)
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let li = document.createElement("li")
+    ul.appendChild(li)
+
+ 
+    li.innerText = input.value;
+    input.value = "";
+
+})
+
 
