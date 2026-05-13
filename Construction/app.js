cc2f8cb3669bdc382f6544a7d9dc89e3a876643e
Gautam Thakur <mnrhag5@gmail.com>
2026-05-13 11:55:15 +0530

    add

--- Construction/app.js
+++ Construction/app.js
@@ -46,20 +46,132 @@
 
 
 
-function Student(name,marks){
-    this.name = name;
-    this.marks = marks;
+// function Student(name,marks){
+//     this.name = name;
+//     this.marks = marks;
+//     this.checkResult = function(){
 
-    if(Student.marks>=40){
-        console.log("Pass");
-    }else{
-        console.log("Fail")
-    }
 
-}
+    
+//     if(this.marks>=40){
+//         console.log("Pass");
+//     }else{
+//         console.log("Fail")
+//     }
 
-let firstStudent = new Student("Gautam",80)
-let secondStudent = new Student("Rohit",20)
+// }}
 
-console.log(firstStudent)
-console.log(secondStudent)
+// let firstStudent = new Student("Gautam",80)
+// let secondStudent = new Student("Rohit",20)
+
+// firstStudent.checkResult();
+// secondStudent.checkResult();
+
+
+
+
+
+//=====Day Two=============================
+//=====Day Two=============================
+//=====Day Two=============================
+//=====Day Two=============================
+//=====Day Two=============================
+//=====Day Two=============================
+//=====Day Two=============================
+
+
+
+
+//=========================question 1
+
+
+// function car(brand,speed){
+//       this.brand = brand;
+//       this.speed = speed;
+ 
+      
+// }
+
+//   car.prototype.showspeed = function(){
+//     console.log(this.brand,"speed is",this.speed)
+//   }
+
+
+
+// let bmw = new car("BMW",200)
+// let tata = new car("TATA",170)
+
+
+// bmw.showspeed()
+
+
+
+
+//===========question 2================
+
+// function Animal(name,sound){
+//     this.name = name;
+//     this.sound = sound;
+// }
+//  Animal.prototype.makeSound = function(){
+//     console.log(this.name,"says",this.sound);
+    
+//  }
+
+//  let Dog = new Animal("Dog","Bark")
+//  let Tiger = new Animal("Tiger","Roar")
+//  let Elephent = new Animal("Elephent","Trumphing")
+ 
+//  Dog.makeSound();
+//  Elephent.makeSound();
+//  Tiger.makeSound();
+
+
+
+
+ //============question 3===================
+
+
+//  function Employee(name,salary){
+//     this.name = name;
+//     this.salary = salary;
+//  }
+
+//  Employee.prototype.showSalary = function(){
+//     console.log(this.name,"salary is" , this.salary)
+//  }
+
+//  let employee1 = new Employee("gautam",50000);
+
+//  employee1.showSalary();
+
+
+ //=============question 4======================
+
+
+
+// function Book(title,author){
+//     this.title = title;
+//     this.author = author;
+
+// }
+ 
+//   Book.prototype.details = function(){
+//     console.log(this.title , "by", this.author);
+    
+//   }
+
+//   let name = new Book("Atomic","James Clear");
+
+//   name.details();
+
+
+
+  let arr = [1,2,3];
+//   console.log(arr.prototype)
+  console.log(arr.__proto__)
+
+
+
+
+
