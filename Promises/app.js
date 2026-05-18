b2bac43c446db70bb62fee15fec31504b7d516b0
Gautam Thakur <mnrhag5@gmail.com>
2026-05-18 19:40:34 +0530

    add

--- Promises/app.js
+++ Promises/app.js
@@ -127,14 +127,156 @@
 
 //----------------------------question----------------------------------------
 
-function hello(message,time){
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{
-            res(message)
-        },time)
-    })
+// function hello(message,time){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//             res(message)
+//         },time)
+//     })
+// }
+// hello("message",2000)
+// .then((data)=>{
+//     console.log(data)
+//     return hello("Login",3000)})
+// .then((data)=>{
+//     console.log(data)
+//     return hello("Fetch Profile",4000)})
+// .then((data)=>{
+//     console.log(data)
+//     return hello("Shoe dashboard",5000)})
+
+// .then((data) => {
+//     console.log(data);
+// });
+
+
+
+//--------------------question---------------------------------------
+
+// function hello(password){
+//     return new Promise((res,rej)=>{
+//         if(password===1456){
+//             res("login successfully")
+//         }else{
+//     rej("wrong password")
+//         }
+//     })
+// }
+// hello(1456)
+// .then((data)=>{
+//     console.log(data)
+// })
+// .catch((error)=>{
+//     console.log(error)
+// })
+
+
+//--------------question-------------
+
+// function hello(){
+//    return new Promise((res)=>{
+//         setTimeout(()=>{
+//             res("data loaded")
+//         },2000)
+//     })
+// }
+// hello()
+// .then((data)=>{console.log(data)})
+
+
+//---------------------question---------------------
+//  function password(value){
+//     return new Promise((res,rej)=>{
+//         if(value==456789){
+//             res("correct")
+//         }else{
+//             rej("wrong password")
+//         }
+//     })
+//  }
+//  password(456789)
+//  .then((data)=>{console.log(data)})
+//  .catch((error)=>{console.log(error)})
+
+
+//-------------------question--------------------------
+
+
+// function chaining(message,time){
+//     return new Promise((res)=>{
+//         setTimeout(()=>{
+//             res(message)
+          
+//         },time)
+//     })
+// }
+// chaining("login",2000)
+// .then((data)=>{console.log(data);return chaining("fetch profile",3000)})
+// .then((data)=>{console.log(data);return chaining("open dashboard",4000)})
+// .then((data)=>{console.log(data)})
+
+//---------------------------------question ------------------------
+
+// function api(){
+//     return new Promise((res)=>{
+//         setTimeout(async()=>{
+       
+//                 let question = await fetch("https://jsonplaceholder.typicode.com/users")
+//              let data = await question.json()
+//                 res(data)
+             
+//             }
+//         ,3000)
+//     })
+// }
+
+// api()
+// .then((data)=>{console.log(data)})
+
+
+
+//-------------------------question--------------------------------------
+
+
+
+// function hello(message,time){
+//     return new Promise((res)=>{
+//         setTimeout(()=>{
+//             res(message)
+//         },3000)
+//     })
+// }
+
+// Promise.all([
+//     hello("hello",2000),
+//     hello("am coming after 3 sec",2000),
+//     hello("and i am after 2 sec",2000),
+//     hello("and me 4 sec",2000),
+// ])
+// .
+// then((data)=>{
+//     console.log(data);
+    
+// })
+
+function task(message, time){
+
+    return new Promise((res) => {
+
+        setTimeout(() => {
+            res(message);
+        }, time);
+
+    });
+
 }
-hello("message",2000)
-.then((data)=>{return hello("Login")})
-.then((data)=>{return hello("Fetch Profile")})
-.then((data)=>{return hello("Shoe dashboard")})
+
+Promise.all([
+    task("Login", 2000),
+    task("Fetch Profile", 3000),
+    task("Dashboard", 1000)
+])
+
+.then((data) => {
+    console.log(data);
+});
