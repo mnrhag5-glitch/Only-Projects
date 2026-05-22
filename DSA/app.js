704edfef779d980a10514231360f1b0d47cbec28
Gautam Thakur <mnrhag5@gmail.com>
2026-05-22 20:41:04 +0530

    update

---
+++ DSA/app.js
@@ -0,0 +1,48 @@
+//here is list ki hm kya start krnge or kis line se---------------
+
+//-----besics----------------->=== 7 line;
+
+
+// a = 223
+// console.log(a);
+
+
+
+// let a = 12;
+// let b = "12";
+// console.log(typeof(a+b))
+
+
+// let c = 23;
+// console.log(c)
+
+// let d = "89"
+// console.log(d)
+
+
+// console.log("sum of a and c is "  , (a+c))
+
+
+
+//------>> 
+//console.log("1"-1)--->s engine ise string ko number me convert kr deta hai
+//---->  or ise ******* TYPEQURSION *****KEHTE HIA
+
+//console.log("1"+1)--->js engine ise string ko number me convert kr deta hai
+
+//("1"+1)--> 11 JAB EK STRING HOTI TO VO CONCATINATE KEHLATA HAI
+
+//(1+)---> YE ADD KEHLATA HAI
+
+
+
+// let age = Number(prompt('enter your age'))
+// console.log(age);
+
+
+//prompt hme sr\tring return krta hai
+
+// jab ek type se dusre type me convert krte hai to use *******TYPECASTING ***** kehte hai
+
+
+
