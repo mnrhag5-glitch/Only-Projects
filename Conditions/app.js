2c054e7e21cc8ff02f6116bf270858cb3bd400c4
Gautam Thakur <mnrhag5@gmail.com>
2026-05-25 19:39:54 +0530

    madd

---
+++ Conditions/app.js
@@ -0,0 +1,14 @@
+// let age = Number(prompt("enter your age"));
+// if(isNaN(age)){
+  //  console.log("wrong input"); // ----isNAN check krega ki ane wali value number hai ya nhi
+    
+// }
+// else if(age>=18){
+//     console.log("you can vote");
+// }else{
+//     console.log("you can not vote");
+// }
+// console.log(typeof(age));
+
+
+
