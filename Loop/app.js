9e92fe9befcc6ae8c3feef42b8b3410ca16415da
Gautam Thakur <mnrhag5@gmail.com>
2026-05-27 17:29:21 +0530

    add

---
+++ Loop/app.js
@@ -0,0 +1,206 @@
+//loop -> whatsapp , insta , facebook , ecommerce ,
+// koi bhi web app me loop use hota hai.
+
+
+
+//*****************jab hme pta ho kitni baar chla h tb for use kro--*********************
+//*****************jab hme pta ho kitni baar chla h tb for use kro--*********************
+//*****************jab hme pta ho kitni baar chla h tb for use kro--*********************
+//*****************jab hme pta ho kitni baar chla h tb for use kro--*********************
+
+
+// for , while , do - while,
+
+// for (start; end ; change){
+//}
+
+// for(i=1; i<20; i++){
+//     console.log(i);
+    
+// }
+
+// for(a=100; a>9; a--){
+//     console.log(a);
+    
+// }
+
+
+
+
+//--------sum of n natural number-----------------------------------------------
+
+
+
+// var pr = prompt("koun sa number add karwaoge")
+//if(pr === null){
+//console.log("cancelled");
+//}
+//else{
+    //var n = Number(pr)
+
+// if(isNaN(n)){
+//     console.log("Invalid Number");
+// }
+// else{
+//     if(n>0){
+       
+// let sum = 0
+// for(i=0; i<=n; i++){
+//   sum = sum + i;
+// }
+// console.log("sum of N number is = ", sum);
+
+        
+//     }else{
+//         console.log("Input value should be More then 0");
+        
+//     }
+// }}
+
+
+
+//--------------------------------------------------------------------------
+
+
+// let n = Number(prompt("koun sa number add karwaoge"))
+
+// let sum = 0;
+// for(i=0; i<=n;i++){
+//    sum = sum + i
+// }
+// console.log(sum);
+
+
+
+//-----------------factoril nikalo-------------------------------------
+
+
+
+// var pr = prompt("koun sa number add karwaoge")
+// if(pr === null){
+// console.log("cancelled");
+// }
+// else{
+//     var n = Number(pr)
+
+// if(isNaN(n)){
+//     console.log("Invalid Number");
+// }
+// else{
+//     if(n>0){
+       
+//     for(i=1; i<=n; i++){
+//         if(n%i===0){
+//             console.log(i);
+            
+//         }
+//     }
+    
+   
+//     }else{
+//         console.log("Input value should be More then 0");
+        
+//     }
+// }}
+
+
+
+//-------------------------prime Number--------------------------------------
+
+
+
+// var pr = prompt("koun sa number add karwaoge")
+// if(pr === null){
+// console.log("cancelled");
+// }
+// else{
+//     var n = Number(pr)
+
+// if(isNaN(n)){
+//     console.log("Invalid Number");
+// }
+// else{
+//     if(n>0){
+//        let primeHai = "true"
+//        for(i=2; i<=Math.floor(n/2); i++)
+//         if(n%i===0){
+//             primeHai=false;
+//             break;
+//         }
+
+// console.log(primeHai);
+
+        
+//     }else{
+//         console.log("Input value should be More then 0");
+        
+//     }
+// }}
+
+
+//----------------BREAK AND CONTINUE--------------------------------
+
+// for (var i=1; i<23; i++){
+//     if(i===11) break;    // vnhi rok dega 10 pr
+//     else console.log(i);
+    
+// }
+
+// for (var i=1; i<23; i++){
+//     if(i===11) continue;     // 11 chod dega baki sb ayega
+//     else console.log(i);
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------while---------------------------------
+//-----------------------while---------------------------------
+//-----------------------while---------------------------------
+//-----------------------while---------------------------------
+//-----------------------while---------------------------------
+//-----------------------while---------------------------------
+
+
+// while tb case m use hota jb tk koi number hme mil na jaye wali sitution ho
+//situation me whiile zyada use hota hai*********
+//situation me whiile zyada use hota hai*********
+//situation me whiile zyada use hota hai*********
+
+
+
+// start;
+// while(end){
+//     change
+// }
+
+
+// var i =0;
+// while(i<20){
+//     console.log(i);
+//     i++;
+    
+// }
+
+
+let num = Number(prompt("enter number"))
+if(isNaN(num)){
+    console.log("Invalid Number");
+}else{
+let sum = 0;
+let i = 0;
+while(i<=num){
+    sum = sum + i 
+//   console.log('sum of your giving', sum);
+  i++;
+  
+}
+   console.log("Final Sum =", sum);}
