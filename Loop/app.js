76ec2d96180b339e009833a72b3f946b25d745c9
Gautam Thakur <mnrhag5@gmail.com>
2026-05-27 19:03:41 +0530

    add

--- Loop/app.js
+++ Loop/app.js
@@ -191,16 +191,111 @@
 // }
 
 
-let num = Number(prompt("enter number"))
-if(isNaN(num)){
-    console.log("Invalid Number");
-}else{
-let sum = 0;
-let i = 0;
-while(i<=num){
-    sum = sum + i 
-//   console.log('sum of your giving', sum);
-  i++;
+//--------------------sum of digit-------------------------------
+
+// let num = Number(prompt("enter number"))
+// if(isNaN(num)){
+//     console.log("Invalid Number");
+// }else{
+// let sum = 0;
+// let i = 0;
+// while(i<=num){
+//     sum = sum + i 
+// // 5
+
+//   i++;
   
-}
-   console.log("Final Sum =", sum);}
+// }
+//    console.log("Final Sum =", sum);}
+
+
+
+//-----------do while-----------------------------------
+//-----------do while-----------------------------------
+//-----------do while-----------------------------------
+//-----------do while-----------------------------------
+
+
+// do{
+//     console.log("hello");
+//     i++
+    
+// }while(12>13)
+
+
+//----------------------guess the number-----------------------------
+
+
+// let random = Math.floor(Math.random()*100)+1;
+
+// let userGuess = 0;
+ 
+//  while(
+//     userGuess!==random
+//  ){
+//     userGuess = Number(prompt("enter number b/w 1-100"))
+
+
+//  if(isNaN(userGuess) || userGuess>100 || userGuess<1){
+//     console.log("enter Valid Number or b/w 1-100")
+
+//  }
+
+//  else if(userGuess>random){
+//     console.log("Number is too high")
+//  }else if(userGuess<random){
+//     console.log("Number is too low")
+//  }else{
+//     console.log("Congrats 🎉🎉 and Number was",userGuess);
+    
+//  }
+//  }
+
+
+
+
+// let random = Math.floor(Math.random() * 100) + 1;
+// let userGuess = 0;
+
+// while(userGuess !== random){
+
+//     userGuess = +prompt("Enter number b/w 1-100");
+
+//     if(isNaN(userGuess) || userGuess < 1 || userGuess > 100){
+//         console.log("Enter valid number");
+//         continue;
+//     }
+//      if(userGuess > random){
+//         console.log("Too High");
+//     }
+//     else if(userGuess < random){
+//         console.log("Too Low");
+//     }
+//     else{
+//         console.log("Congrats 🎉 Number was", random);
+//     }
+// }
+
+
+
+// let random = Math.floor(Math.random() * 100) + 1;
+// let userGuess = 0;
+// while(userGuess !== random){
+
+//     userGuess = Number(prompt("Enter number b/w 1-100"));
+
+//     if(isNaN(userGuess) || userGuess < 1 || userGuess > 100){
+//         console.log("Enter Valid Number");
+//     }
+//     else if(userGuess > random){
+//         console.log("Too High");
+//     }
+//     else if(userGuess < random){
+//         console.log("Too Low");
+//     }
+//     else{
+//         console.log("Congrats 🎉 Number was", random);
+//     }
+// }
+
+console.log("and Fuck You");
