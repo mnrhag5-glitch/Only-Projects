075b3471cd13c0bf86991ffb769d7d3171c909e1
Gautam Thakur <mnrhag5@gmail.com>
2026-05-28 20:48:05 +0530

    add

--- I-Questions/app.js
+++ I-Questions/app.js
@@ -14,17 +14,26 @@
 
 //************    80 se upr number print kro  ********************** */
 
-let obj = [{
-    name:"gautam",marks:89},
-    {name:"seju",marks:90},
-    {name:"luffy",marks:79},
-    {name:"zoro",marks:69}
-]
+// let obj = [{
+//     name:"gautam",marks:89},
+//     {name:"seju",marks:90},
+//     {name:"luffy",marks:79},
+//     {name:"zoro",marks:69}
+// ]
 
-let newObj = obj.filter((i)=>{
-    return i.marks>80
-})
-console.log(newObj);
+// let newObj = obj.filter((i)=>{
+//     return i.marks>80
+// })
+// console.log(newObj);
+
+
+
+//
+
+
+console.log(isNaN("harshit"));
+
+
 
 
 
